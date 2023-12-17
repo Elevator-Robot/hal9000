ec45bb2639c31a70f332ff21112130c21ff380dd
billy <aphexlog@gmail.com>
2023-12-16 18:52:34 -0600

    blahh

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import aws_cdk.aws_chatbot as chatbot
 import aws_cdk.aws_sns as sns
 from constructs import Construct
 
+from aws_cdk import (
+    aws_codepipeline as codepipeline,
+    aws_codepipeline_actions as codepipeline_actions,
+    aws_codebuild as codebuild,
+)
+
 app = cdk.App()
 
 
@@ -33,6 +39,62 @@ class Hal9000Stack(cdk.Stack):
         )
 
 
-Hal9000Stack(app, "Hal9000Stack")
+class PipelineStack(cdk.Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        source_output = codepipeline.Artifact()
+        build_output = codepipeline.Artifact()
+
+        codebuild_project = codebuild.PipelineProject(
+            self,
+            "Hal9000CodeBuildProject",
+            build_spec=codebuild.BuildSpec.from_source_filename(
+                filename="buildspec.yml"
+            ),
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.STANDARD_5_0
+            ),
+        )
+
+        codepipeline.Pipeline(
+            self,
+            "Hal9000Pipeline",
+            stages=[
+                codepipeline.StageProps(
+                    stage_name="Source",
+                    actions=[
+                        codepipeline_actions.GitHubSourceAction(
+                            action_name="GitHub_Source",
+                            owner="elevator-robot",
+                            repo="hal9000",
+                            branch="dev",
+                            trigger=codepipeline_actions.GitHubTrigger.WEBHOOK,
+                            oauth_token=cdk.SecretValue.unsafe_plain_text(
+                                "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"
+                            ),
+                            output=source_output,
+                        )
+                    ],
+                ),
+                codepipeline.StageProps(
+                    stage_name="Build",
+                    actions=[
+                        codepipeline_actions.CodeBuildAction(
+                            action_name="CodeBuild",
+                            project=codebuild_project,
+                            input=source_output,
+                            outputs=[build_output],
+                        )
+                    ],
+                ),
+            ],
+        )
+
+        # The code that defines your stack goes here
+        cdk.CfnOutput(self, "PipelineStack", value="PipelineStack")
+
+
+PipelineStack(app, "PipelineStack")
 
 app.synth()
